9bd792701cf2ae79f30a662bacae5d506b8ea6ec
René Sackers <rene.sackers@live.com>
2017-05-25 23:43:30 +0200

    Only update once per second.

--- compass/compass.js
+++ compass/compass.js
@@ -1,57 +1,56 @@
-var x;
-var y;
 var text;
 var zone;
 var street;
 
-API.onUpdate.connect(function (sender) {
+var updateTimeoutInMilliseconds = 1000;
+var lastUpdateTickCount = 0;
 
-    x = API.getGameplayCamDir().X;
-    y = API.getGameplayCamDir().Y;
-    zone = API.getZoneNameLabel(API.getEntityPosition(API.getLocalPlayer()));
-    street = API.getStreetName(API.getEntityPosition(API.getLocalPlayer()));
-
-    if (0.3 < x && 0.3 < y) {
-
-        text="NE";
-
-    }
-
-    else if (x<-0.3 && 0.3 < y) {
-
-        text="NW";
-    }
-
-    else if (0.3 < x && y < -0.3) {
-
-        text="SE";
-    }
-
-    else if (x < -0.3 && y < -0.3) {
-
-        text="SW";
-    }
-
-    else if (-0.3 < x && x < 0.3 && y < -0.3) {
-
-        text="S";
-    }
-
-    else if (x < -0.3 && -0.3 < y && y < 0.3) {
-
-        text="W";
-    }
-
-    else if (0.3 < x && -0.3 < y && y < 0.3) {
-
-        text="E";
-    }
-
-    else if (-0.3 < x && x < 0.3 && y > 0.3) {
-
-        text="N";
-    }
+function updateDirectionText() {
+    var cameraDirection = API.getGameplayCamDir();
     
+    if (0.3 < cameraDirection.X && 0.3 < cameraDirection.Y) {
+        text = "NE";
+    }
+    else if (cameraDirection.X < -0.3 && 0.3 < cameraDirection.Y) {
+        text = "NW";
+    }
+    else if (0.3 < cameraDirection.X && cameraDirection.Y < -0.3) {
+        text = "SE";
+    }
+    else if (cameraDirection.X < -0.3 && cameraDirection.Y < -0.3) {
+        text = "SW";
+    }
+    else if (-0.3 < cameraDirection.X && cameraDirection.X < 0.3 && cameraDirection.Y < -0.3) {
+        text = "S";
+    }
+    else if (cameraDirection.X < -0.3 && -0.3 < cameraDirection.Y && cameraDirection.Y < 0.3) {
+        text = "W";
+    }
+    else if (0.3 < cameraDirection.X && -0.3 < cameraDirection.Y && cameraDirection.Y < 0.3) {
+        text = "E";
+    }
+    else if (-0.3 < cameraDirection.X && cameraDirection.X < 0.3 && cameraDirection.Y > 0.3) {
+        text = "N";
+    }
+}
+
+function updateValues() {
+    var playerPosition = API.getEntityPosition(API.getLocalPlayer());
+    
+    zone = API.getZoneNameLabel(playerPosition);
+    street = API.getStreetName(playerPosition);
+
+    updateDirectionText();
+}
+
+API.onUpdate.connect(function (sender) {
+    var currentTimeInMilliseconds = new Date().getTime();
+    if (currentTimeInMilliseconds - lastUpdateTickCount > updateTimeoutInMilliseconds)
+    {
+        lastUpdateTickCount = currentTimeInMilliseconds;
+        updateValues();
+    }
+
     API.drawText(text, 350, 1000, 1, 255, 255, 255, 200, 2, 1, false, true, 0);
     API.drawText("|", 385, 1000, 1, 255, 255, 255, 200, 4, 1, false, true, 0);
     API.drawText(zone, 425, 1003, 0.5, 255, 255, 255, 200, 4, 1, false, true, 0);
